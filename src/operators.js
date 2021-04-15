88ba5d97ec4150573fd1ac03d0f9b10991d30753
Jocelyn Badgley <jbadgley@clarocity.com>
2021-04-15 14:54:52 -0700

    Cleaning up some operators

--- src/operators.js
+++ src/operators.js
@@ -3,6 +3,7 @@ import {
   isObject,
   isArray,
   isNumeric,
+  isString,
   sum,
   avg,
   median,
@@ -30,54 +31,57 @@ function randomItem (items) {
 
 const push = (arr, ...items) => { arr.push(...items); return arr; };
 
+function bool (input) { return  input ? [ true ] : []; }
+
 export const DEFAULT_OPERATORS = {
   '*':        [ 1, (set) => set.reduce((items, item) => (isMappable(item) ? push(items, ...values(item)) : push(items, item)), []) ],
   '~':        [ 1, (set) => set.reduce((items, item) => (isMappable(item) ? push(items, ...keys(item)) : push(items, item)), []) ],
 
-  '===':      [ 0, ([ a ], [ b ]) => [ a === b ] ],
-  '==':       [ 0, ([ a ], [ b ]) => [ a ==  b ] ], // eslint-disable-line eqeqeq
-  '!==':      [ 0, ([ a ], [ b ]) => [ a !== b ] ],
-  '!=':       [ 0, ([ a ], [ b ]) => [ a !=  b ] ], // eslint-disable-line eqeqeq
-  '<=':       [ 0, ([ a ], [ b ]) => [ a <=  b ] ],
-  '<':        [ 0, ([ a ], [ b ]) => [ a <   b ] ],
-  '>=':       [ 0, ([ a ], [ b ]) => [ a >=  b ] ],
-  '>':        [ 0, ([ a ], [ b ]) => [ a >   b ] ],
-  '-':        [ 0, ([ a ], [ b ]) => [ a - b ] ],
-  '+':        [ 0, ([ a ], [ b ]) => [ a + b ] ],
+  '===':      [ 0, ([ a ], [ b ]) => bool(a === b) ],
+  '==':       [ 0, ([ a ], [ b ]) => bool(a  == b) ], // eslint-disable-line eqeqeq
+  '!==':      [ 0, ([ a ], [ b ]) => bool(a !== b) ],
+  '!=':       [ 0, ([ a ], [ b ]) => bool(a !=  b) ], // eslint-disable-line eqeqeq
+  '<=':       [ 0, ([ a ], [ b ]) => bool(a <=  b) ],
+  '<':        [ 0, ([ a ], [ b ]) => bool(a  <  b) ],
+  '>=':       [ 0, ([ a ], [ b ]) => bool(a >=  b) ],
+  '>':        [ 0, ([ a ], [ b ]) => bool(a  >  b) ],
+  '-':        [ 0, ([ a ], [ b ]) => [ Number(a)  -  Number(b) ] ],
+  '+':        [ 0, ([ a ], [ b ]) => [ Number(a)  +  Number(b) ] ],
 
   '&&':       [ 0, (seta, setb) => (seta.filter(truthy).length ? setb : seta) ],
   '||':       [ 0, (seta, setb) => (seta.filter(truthy).length ? seta : setb) ],
   '??':       [ 0, (seta, setb) => ( seta.length ? seta : setb ) ],
 
-  '!':        [ -1, (set) => (set.filter(falsey).length ? [] : [ true ]) ],
+  '!!':       [ -1, (set) => bool( set.filter(falsey).length) ],
+  '!':        [ -1, (set) => bool(!set.filter(falsey).length) ],
 
   is:    [ 0, (seta, setb) => {
     const m = intersect(seta, setb).length;
-    return m === seta.length && m === setb.length;
+    return bool(m === seta.length && m === setb.length);
   } ],
   in:         [ 0, (seta, setb) => intersect(seta, setb) ],
   not:        [ 0, (seta, setb) => difference(seta, setb) ],
   subset:     [ 0, (seta, setb) => (intersect(seta, setb).length === seta.length ? seta : []) ],
-  size:       [ 0, (seta, setb)   => [ seta.length === setb.length ] ],
-  typeof:     [ 0, ([ a ], [ b ]) => [ typeof a === typeof b ] ],
-  ntypeof:    [ 0, ([ a ], [ b ]) => [ typeof a !== typeof b ] ],
+  size:       [ 0, (seta, setb)   => bool(seta.length === setb.length) ],
+  typeof:     [ 0, ([ a ], [ b ]) => bool(typeof a === typeof b) ],
+  ntypeof:    [ 0, ([ a ], [ b ]) => bool(typeof a !== typeof b) ],
   mod:        [ 0, (set, [ b ])   => set.map((a) => a % b) ],
-  nmod:       [ 0, (set, [ b ])   => set.map((a) => a % b === 0) ],
   pow:        [ 0, (set, [ b ])   => set.map((a) => Math.pow(a, b)) ],
   join:       [ 0, (set, [ del ]) => [ set.join(del) ] ],
+  split:      [ 0, (set, [ del ]) => set.reduce((results, a) => ( isString(a) ? push(results, a.split(del)) : results)) ],
   add:        [ 0, (set, [ b ])   => set.map((a) => a + b) ],
   sub:        [ 0, (set, [ b ])   => set.map((a) => a - b) ],
   mul:        [ 0, (set, [ b ])   => set.map((a) => a * b) ],
   div:        [ 0, (set, [ b ])   => set.map((a) => a / b) ],
 
-  keys:       [ -1, (set) => set.reduce((items, item) => push(items, ...keys(item)), []) ],
-  values:     [ -1, (set) => set.reduce((items, item) => push(items, ...values(item)), []) ],
+  keys:       [ -1, (set) => set.reduce((items, item) => (isMappable(item) ? push(items, ...keys(item)) : items), []) ],
+  values:     [ -1, (set) => set.reduce((items, item) => (isMappable(item) ? push(items, ...values(item)) : items), []) ],
   abs:        [ -1, (set) => set.map(Math.abs) ],
   ceil:       [ -1, (set) => set.map(Math.ceil) ],
   floor:      [ -1, (set) => set.map(Math.floor) ],
   round:      [ -1, (set) => set.map(Math.round) ],
-  min:        [ -1, (set) => [ Math.min(...set) ] ],
-  max:        [ -1, (set) => [ Math.max(...set) ] ],
+  min:        [ -1, (set) => [ Math.min(...set.filter(isNumeric)) ] ],
+  max:        [ -1, (set) => [ Math.max(...set.filter(isNumeric)) ] ],
   sum:        [ -1, (set) => [ sum(set.filter(isNumeric)) ] ],
   avg:        [ -1, (set) => [ avg(set.filter(isNumeric)) ] ],
   med:        [ -1, (set) => [ median(set.filter(isNumeric)) ] ],
@@ -86,12 +90,12 @@ export const DEFAULT_OPERATORS = {
   first:      [ -1, (set) => [ set[0] ] ],
   last:       [ -1, (set) => [ set[set.length - 1] ] ],
   unique:     [ -1, (set) => uniq(set) ],
-  any:        [ -1, (set) => (any(set) ? [ true ] : []) ],
-  all:        [ -1, (set) => (all(set) ? [ true ] : []) ],
-  none:       [ -1, (set) => (none(set) ? [ true ] : []) ],
-  sizeof:     [ -1, ([ a ]) => [ sizeOf(a) ] ],
+  any:        [ -1, (set) => bool(any(set)) ],
+  all:        [ -1, (set) => bool(all(set)) ],
+  none:       [ -1, (set) => bool(none(set)) ],
+  sizeof:     [ -1, (set) => set.map(sizeOf) ],
   count:      [ -1, (set) => [ set.length ] ],
-  empty:      [ -1, (set) => (set.length ? [] : [ true ]) ],
+  empty:      [ -1, (set) => bool(set.length) ],
 };
 
 export function parseOperators (ops) {
